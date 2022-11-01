d1d73729f3000d87fd89928324b06f300131e52b
KhaPN <khamin0503@gmail.com>
2022-11-01 12:08:38 +0700

    change to php

--- index.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-const port = process.env.PORT || 5000;
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Welcome to GeeksforGeeks");
-}).listen(port);
